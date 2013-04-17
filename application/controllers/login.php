c9ca23454b5b8b08f0354f75c4d87846e276c25f
Bryanwp <bwp.pearson@gmail.com>
2013-04-16 23:13:16 -0400

    worked out login page, needs js part completed

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -5,155 +5,65 @@ if (!defined('BASEPATH'))
 
 class Login extends CI_Controller {
 
-  
-      public function Admin()
-    {
+    public function Login() {
+        //over ride all other functions with required base url
         parent::__construct();
         $this->load->helper('url');
     }
 
-    public function index()
-    {
+    function index() {
+        //check for logged in session
         $is_logged_in = $this->session->userdata('is_logged_in');
-        if(!isset ($is_logged_in) || $is_logged_in != true)
-        {
-            $this->load->helper('form');
-            $data['main_content'] = 'login';
-            $this->load->view('maintemplate/jointemplate', $data);        
-            
-        }
-        else
-        {
-            redirect('upload');
-        }
-    }
+        //if person has cookie set, and has a value we expect, automatically log them in
+        if (!isset($is_logged_in) || $is_logged_in != true) {
+            //no cookie set for user or not as expected , load validation and rules
+            $this->load->library('form_validation');
+            //set delimiters for alert box
+            $this->form_validation->set_error_delimiters('<div class="alert alert-dejami">', '</div>');
+            $this->form_validation->set_rules('username', 'Username', 'required|trim|xss_clean');
+            $this->form_validation->set_rules('password', 'Password Confirmation', 'required|trim|md5|min_length[5]');
 
-    function login()
-    {
-        $this->load->helper('url');
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('user_name', 'Username', 'trim|required|xss_clean');
-        $this->form_validation->set_rules('pass_word', 'Password', 'trim|required');
+            //run validation following form submission
+            if ($this->form_validation->run() == FALSE) {
+                //failed to pass validation
+                //reload login form 
+                $data['main_content'] = 'login';
+                $this->load->view('maintemplate/jointemplate', $data);
+            } else {
+                // pass validation but needs checking against the db
+                //loading datbase helper
+                $this->load->database();
+                $this->load->model('user_verify');
+                $query = $this->user_verify->login();
 
-        if ($this->form_validation->run() == FALSE)
-        {
-            //this person didnt validate
-            $this->index();
-        }
-
-        else
-        {
-            //validated but needs checking against the db
-            $this->load->database();
-            $this->load->model('user_verify');
-            $query = $this->user_verify->login();
-
-            if($query)
-            {
-                $data = array(
+                if ($query) {
+                    $data = array(
                         'username' => $this->input->post('user_name'),
                         'is_logged_in' => true
-                );
-                $this->session->set_userdata($data);
-                redirect('active');
+                    );
+                    $this->session->set_userdata($data);
+                    //great success - validated form and exist in the database
+                    redirect('upload');
+                } else {
+                    //failed db lookup
+                    $data['main_content'] = 'login';
+                    $this->load->view('maintemplate/jointemplate', $data);
+                }
             }
-            else
-            {
-                $this->index();
-            }
-        }
-
-    }
-
-
-    public function logout()
-    {
-        $this->session->sess_destroy();
-        $this->index();
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public function index() {
-       
-        //user is still actively logged in, no reason to bother 
-        $is_logged_in = $this->session->userdata('is_logged_in');
-         if(!isset ($is_logged_in) || $is_logged_in != true)
-        {
-            redirect('upload');
-            
-        }
-        
-        $this->load->helper('form');
-             
-        $this->load->library('form_validation');
-        $this->form_validation->set_error_delimiters('<div class="alert alert-dejami">', '</div>');
-        $this->form_validation->set_rules('username', 'Username', 'required');
-        $this->form_validation->set_rules('password', 'Password Confirmation', 'required|trim|md5|min_length[5]');
-        //$this->form_validation->set_rules('ipaddresss','ip address','valid_ip');
-
-        if ($this->form_validation->run() == FALSE) {
-            //validation has not run/information passed is not valid yet
-            $data['main_content'] = 'login';
-            $this->load->view('maintemplate/jointemplate', $data);        
-                             
-                       
         } else {
-            //passed validation - checking datastore for results
-            
-            
-            $data['main_content'] = 'login';
-            $this->load->view('maintemplate/jointemplate', $data);     
+            //have cookie and have expected values set
+            redirect('upload');
         }
-        
-
-        
-
-//$this->load->view('maintemplate/base', $data);
     }
 
-    public function logout(){
+    public function logout() {
         $this->session->sess_destroy();
         $this->index();
-        
     }
-    
+
 }
-     
-*/
-}
+
+
 /* End of file login.php */
 /* Location: ./application/controllers/login.php */
+    
